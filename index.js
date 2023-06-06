bc463e4d0755ead15eafe32136d262690c808b9f
Mohammad Rahman <me@shadikur.com>
2023-06-06 18:58:18 +0200

    Added verify JWT

--- index.js
+++ index.js
@@ -9,6 +9,24 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+// Verify JWT Token
+const verifyJWT = (req, res, next) => {
+    const token = req.headers["authorization"];
+    const accessToken = token && token.split(" ")[1];
+    if (!accessToken) {
+        res.status(401).send({ auth: false, message: "Unauthorized Access" });
+    } else {
+        jwt.verify(accessToken, process.env.JWT_SECRET, (err, decoded) => {
+            if (err) {
+                res.status(403).json({ auth: false, message: "Access Forbidden!" });
+            } else {
+                req.userId = decoded.id;
+                next();
+            }
+        });
+    }
+};
+
 
 
 // Routes
