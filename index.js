f7baba639820dfb6cb049738901c3b06c9a60670
Mohammad Rahman <me@shadikur.com>
2023-06-10 10:59:46 +0200

    Package and routes updated

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const jwt = require('jsonwebtoken');
 const app = express();
 const port = process.env.PORT || 5000;
@@ -26,6 +27,58 @@ const verifyJWT = (req, res, next) => {
         });
     }
 };
+
+
+// MongoDB Connection
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@creativeexpressions.w7kftro.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+// MongoDB Collection
+const appDB = client.db(process.env.DB_NAME);
+const users = appDB.collection("users");
+const events = appDB.collection("events");
+const classes = appDB.collection("classes");
+const payments = appDB.collection("payments");
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+        // create user to users collection
+        app.post('/users', async (req, res) => {
+            const newUser = req.body;
+            const result = await users.insertOne(newUser);
+            console.log(result);
+            res.json(result);
+        });
+
+        app.get('/users', async (req, res) => {
+            const cursor = users.find({});
+            const result = await cursor.toArray();
+            res.json(result);
+        });
+
+
+
+    } finally {
+        // Ensures that the client will close when you finish/error
+        //await client.close();
+    }
+}
+run().catch(console.dir);
+
 
 
 
