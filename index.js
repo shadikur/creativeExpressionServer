308c1cf9cca132da30ad0c258e878726ecb12ad4
Mohammad Rahman <me@shadikur.com>
2023-06-12 13:21:08 +0200

    Index Updated

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ const payments = appDB.collection("payments");
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        // await client.connect();
+        await client.connect();
         // Send a ping to confirm a ssccessful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
